0697c70c5f3dbbe246bd95134abad318286278f7
Abdullah Miraj <miraj2465@gmail.com>
2023-04-15 15:31:36 +0600

    dbConnect updated

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,14 +1,14 @@
 const fs = require("fs");
 
 function dataCollection() {
-  const jsonString = fs.readFileSync("data/userData.json", "utf8");
+  const jsonString = fs.readFileSync("./userData.json", "utf8");
   const data = JSON.parse(jsonString);
   return data;
 }
 
 function addToData(data){
   const dataJson = JSON.stringify(data)
-  fs.writeFileSync("data/userData.json", dataJson);
+  fs.writeFileSync("./userData.json", dataJson);
 }
 
 module.exports = {dataCollection, addToData};
