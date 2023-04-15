be2efc3fba66f8de250f3ca9f2c828431f4f9c14
Abdullah Miraj <miraj2465@gmail.com>
2023-04-15 13:56:04 +0600

    add public, create data folder

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
 const userRouter = require("./routes/v1/user.route.js");
+const path = require('path')
 
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(cors());
 app.use(express.json());
 
